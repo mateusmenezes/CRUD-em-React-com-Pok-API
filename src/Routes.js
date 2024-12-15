22972d7e6db976173549979532f1d4c3212aedb4
=Mateus Helcias <=matheushelciascabr1@gmail.com>
2024-12-15 10:09:49 -0300

    Ediçao da pagina de erro

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/index.js';
 import Home from './pages/Home/index.js';
 import Favoritos from './pages/Favoritos/index.js';
+import Erro from './pages/Erro/index.js';
 
 
 function RoutesApp() {
@@ -12,6 +13,7 @@ function RoutesApp() {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/favoritos' element={<Favoritos />} />
+                <Route path='*' element={<Erro />} />
             </Routes>
         </BrowserRouter>
     );
